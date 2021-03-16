a4d15a0a94219e9609ba6a50a9c86f50cff22720
roast247 <77567356+roast247@users.noreply.github.com>
2021-03-16 00:17:18 -0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.KryptonButton1 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.KryptonButton2 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.KryptonButton3 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.KryptonButton4 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.KryptonButton5 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1})
+        Me.ListView1.ForeColor = System.Drawing.Color.White
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(150, 48)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(264, 158)
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.List
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = ""
+        Me.ColumnHeader1.Width = 260
+        '
+        'KryptonButton1
+        '
+        Me.KryptonButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.KryptonButton1.Location = New System.Drawing.Point(12, 48)
+        Me.KryptonButton1.Name = "KryptonButton1"
+        Me.KryptonButton1.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.KryptonButton1.Size = New System.Drawing.Size(132, 35)
+        Me.KryptonButton1.StateCommon.Back.Color1 = System.Drawing.Color.Black
+        Me.KryptonButton1.StateCommon.Back.Color2 = System.Drawing.Color.Black
+        Me.KryptonButton1.TabIndex = 1
+        Me.KryptonButton1.Values.Text = "Add New File"
+        '
+        'KryptonButton2
+        '
+        Me.KryptonButton2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.KryptonButton2.Location = New System.Drawing.Point(12, 89)
+        Me.KryptonButton2.Name = "KryptonButton2"
+        Me.KryptonButton2.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.KryptonButton2.Size = New System.Drawing.Size(132, 35)
+        Me.KryptonButton2.StateCommon.Back.Color1 = System.Drawing.Color.Black
+        Me.KryptonButton2.StateCommon.Back.Color2 = System.Drawing.Color.Black
+        Me.KryptonButton2.TabIndex = 2
+        Me.KryptonButton2.Values.Text = "Remove Selected Item"
+        '
+        'KryptonButton3
+        '
+        Me.KryptonButton3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.KryptonButton3.Location = New System.Drawing.Point(12, 130)
+        Me.KryptonButton3.Name = "KryptonButton3"
+        Me.KryptonButton3.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.KryptonButton3.Size = New System.Drawing.Size(132, 35)
+        Me.KryptonButton3.StateCommon.Back.Color1 = System.Drawing.Color.Black
+        Me.KryptonButton3.StateCommon.Back.Color2 = System.Drawing.Color.Black
+        Me.KryptonButton3.TabIndex = 3
+        Me.KryptonButton3.Values.Text = "Clear List"
+        '
+        'KryptonButton4
+        '
+        Me.KryptonButton4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.KryptonButton4.Location = New System.Drawing.Point(12, 171)
+        Me.KryptonButton4.Name = "KryptonButton4"
+        Me.KryptonButton4.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.KryptonButton4.Size = New System.Drawing.Size(132, 35)
+        Me.KryptonButton4.StateCommon.Back.Color1 = System.Drawing.Color.Black
+        Me.KryptonButton4.StateCommon.Back.Color2 = System.Drawing.Color.Black
+        Me.KryptonButton4.TabIndex = 4
+        Me.KryptonButton4.Values.Text = "Add Icon"
+        '
+        'KryptonButton5
+        '
+        Me.KryptonButton5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.KryptonButton5.Location = New System.Drawing.Point(159, 212)
+        Me.KryptonButton5.Name = "KryptonButton5"
+        Me.KryptonButton5.PaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.SparkleBlue
+        Me.KryptonButton5.Size = New System.Drawing.Size(116, 35)
+        Me.KryptonButton5.StateCommon.Back.Color1 = System.Drawing.Color.Black
+        Me.KryptonButton5.StateCommon.Back.Color2 = System.Drawing.Color.Black
+        Me.KryptonButton5.TabIndex = 5
+        Me.KryptonButton5.Values.Text = "Bind"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 8.0!)
+        Me.Label1.Location = New System.Drawing.Point(51, 222)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 14)
+        Me.Label1.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 222)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 14)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Status:"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(427, 25)
+        Me.Panel1.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(33, 6)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(116, 14)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Roasting File Binder"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.Roasting_File_Binder_2._0.My.Resources.Resources.file
+        Me.PictureBox1.Image = Global.Roasting_File_Binder_2._0.My.Resources.Resources.file
+        Me.PictureBox1.Location = New System.Drawing.Point(0, -3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(27, 25)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Red
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(402, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(25, 25)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "X"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Airstrike Bold", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(156, 28)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 17)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "File Binder"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.ClientSize = New System.Drawing.Size(427, 255)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.KryptonButton5)
+        Me.Controls.Add(Me.KryptonButton4)
+        Me.Controls.Add(Me.KryptonButton3)
+        Me.Controls.Add(Me.KryptonButton2)
+        Me.Controls.Add(Me.KryptonButton1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.White
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(427, 255)
+        Me.MinimumSize = New System.Drawing.Size(427, 255)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents KryptonButton1 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents KryptonButton2 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents KryptonButton3 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents KryptonButton4 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents KryptonButton5 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,178 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Text
+
+Public Class Form1
+
+    Private BIcon As String = vbNullString
+    Private draggable As Boolean
+    Private mouseY As Integer
+    Private mouseX As Integer
+
+    Public Function Compress(ByVal bytData() As Byte) As Byte()
+        Using oMS As New MemoryStream()
+            Using oGZipStream As New GZipStream(oMS, CompressionMode.Compress)
+                oGZipStream.Write(bytData, 0, bytData.Length)
+                oGZipStream.Close()
+                ReDim bytData(oMS.ToArray.Length - 1)
+                bytData = oMS.ToArray
+            End Using
+            oMS.Close()
+        End Using
+        Return bytData
+    End Function
+
+    Public Function countitems() As Integer
+        Dim num As Integer
+        Dim enumerator As IEnumerator
+        Try
+            enumerator = ListView1.Items.GetEnumerator
+            Do While enumerator.MoveNext
+                Dim current As ListViewItem = DirectCast(enumerator.Current, ListViewItem)
+                num += 1
+            Loop
+        Finally
+            If TypeOf enumerator Is IDisposable Then
+                TryCast(enumerator, IDisposable).Dispose()
+            End If
+        End Try
+        Return num
+    End Function
+    Private Sub KryptonButton1_Click(sender As Object, e As EventArgs) Handles KryptonButton1.Click
+        Try
+            Dim O As New OpenFileDialog
+            With O
+                .Title = "Select An Application To Bind It"
+                .Filter = "Application | *.exe"
+            End With
+            If O.ShowDialog = Windows.Forms.DialogResult.OK Then
+                ListView1.Items.Add(O.FileName)
+                Label1.Text = "- "
+            End If
+        Catch ex As Exception
+            My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Hand)
+        End Try
+    End Sub
+
+    Private Sub KryptonButton2_Click(sender As Object, e As EventArgs) Handles KryptonButton2.Click
+        Try
+            Dim enumerator As IEnumerator
+            Try
+                enumerator = ListView1.SelectedItems.GetEnumerator
+                Do While enumerator.MoveNext
+                    DirectCast(enumerator.Current, ListViewItem).Remove()
+                Loop
+            Finally
+                If TypeOf enumerator Is IDisposable Then
+                    TryCast(enumerator, IDisposable).Dispose()
+                End If
+            End Try
+        Catch ex As Exception
+            My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Hand)
+        End Try
+    End Sub
+
+    Private Sub KryptonButton3_Click(sender As Object, e As EventArgs) Handles KryptonButton3.Click
+        ListView1.Items.Clear()
+    End Sub
+
+    Private Sub KryptonButton4_Click(sender As Object, e As EventArgs) Handles KryptonButton4.Click
+        Try
+            Dim O As New OpenFileDialog
+            With O
+                .Title = "Select An Icon"
+                .Filter = "Icon | *.ico"
+            End With
+            If O.ShowDialog = Windows.Forms.DialogResult.OK Then
+                BIcon = O.FileName
+                Label1.Text = "- "
+            End If
+        Catch ex As Exception
+            My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Hand)
+        End Try
+    End Sub
+
+    Private Sub KryptonButton5_Click(sender As Object, e As EventArgs) Handles KryptonButton5.Click
+        Try
+            Dim Sv As New SaveFileDialog
+            With Sv
+                .Title = "Save The Binding Stub"
+                .Filter = "Application | *.exe"
+                .FileName = "Binded"
+                If Sv.ShowDialog = Windows.Forms.DialogResult.OK Then
+                Dim S As String = Sv.FileName
+                Dim buffer As Byte() = My.Resources.Stub
+                My.Computer.FileSystem.WriteAllBytes(.filename, buffer, False)
+                Dim N As Integer = countitems()
+                ResourceWriter.WriteResource(S, Encoding.UTF7.GetBytes(N), 1)
+                Dim IEnu As IEnumerator
+                IEnu = ListView1.Items.GetEnumerator
+                For i = 0 To N - 1
+                    IEnu.MoveNext()
+                    Dim current As ListViewItem = DirectCast(IEnu.Current, ListViewItem)
+                    ResourceWriter.WriteResource(S, Compress(File.ReadAllBytes(current.Text)), i + 2)
+                Next
+                If BIcon <> "" Then
+                    IconInjector.InjectIcon(S, BIcon)
+                End If
+                Label1.Text = "Binded Successfully"
+            Else
+                Label1.Text = "Binding Failed"
+                My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Asterisk)
+            End If
+            End With
+        Catch ex As Exception
+            My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Hand)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        draggable = False
+    End Sub
+
+    Private Sub Label4_MouseDown(sender As Object, e As MouseEventArgs) Handles Label4.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+    Private Sub Label4_MouseMove(sender As Object, e As MouseEventArgs) Handles Label4.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+    Private Sub Label4_MouseUp(sender As Object, e As MouseEventArgs) Handles Label4.MouseUp
+        draggable = False
+    End Sub
+    Private Sub PictureBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseDown
+        draggable = True
+        mouseX = Cursor.Position.X - Me.Left
+        mouseY = Cursor.Position.Y - Me.Top
+    End Sub
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        If draggable Then
+            Me.Top = Cursor.Position.Y - mouseY
+            Me.Left = Cursor.Position.X - mouseX
+        End If
+    End Sub
+    Private Sub PictureBox1_MouseUp(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseUp
+        draggable = False
+    End Sub
+End Class
